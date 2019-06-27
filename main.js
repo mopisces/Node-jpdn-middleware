5b352d03e33e75b6d56f4c2c8e25d5a0d5e9892b
mopisces <1105734919@qq.com>
2019-06-27 11:57:35 +0800

    20190627

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-//const Koa = require('koa');
+var Koa = require('koa');
 //const cors = require('koa2-cors');
-var httpProxy = require('http-proxy');
+var http = require('http'),
+	httpProxy = require('http-proxy');
 //var http = require('http');
-//const app = new Koa();
+var app = new Koa();
 //app.use(cors());
 
-httpProxy.createProxyServer({
-	target : 'http://127.0.0.1:3030',
-	selfHandleResponse: true
-}).listen(3000);
+var proxy = httpProxy.createProxyServer({});
+proxy.on('proxyReq',function(proxyReq,req,res,options){
+	if(req.method == 'OPTIONS'){
+		console.dir('OPTIONS');
+	}
+	console.dir('get');
+	proxyReq.setHeader('referer','http://127.0.0.1:3000');
+})
 
+var server = http.createServer(function(req,res){
+	proxy.web(req,res,{target:'http://127.0.0.1:3030'});
+})
 /*http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'});
 	res.write( 'request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2) );
@@ -23,13 +31,29 @@ httpProxy.createProxyServer({
 //var proxy = httpProxy.createProxyServer({});
 
 
-proxy.on('proxyRes',function(proxyRes,req,res){
+
+/*proxy.on('proxyRes',function(proxyRes,req,res){
 	
-})
+})*/
 //proxy.web(req,res,options);
 /*var server = http.createServer(function(req, res) {
 	proxy.web(req,res,options);
 	//proxy.web(req, res, { target: 'http://127.0.0.1:3030' });
 });*/
-//server.listen(3000);
+server.listen(3000);
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
+});
+
 //app.listen(3000);
+//
+//{"accept-language":"zh-CN,zh;q=0.9",
+//"accept-encoding":"gzip, deflate, br",
+//"referer":"http://localhost:8081/login",
+//"user-agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+//"authentication":"userInfo","origin":"http://localhost:8081",
+//"accept":"application/json, text/plain, */*",
+//"connection":"close",
+//"host":"127.0.0.1:3000",
+//"x-special-proxy-header":"helloworld"}
